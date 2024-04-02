94b134a510d09df3b7d27129a0860334355913f5
gilvania moreira <moreiragil84@gmail.com>
2024-04-02 07:19:13 -0300

    feat: css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Header } from './components/Header';
+import { Post } from './Post'
 
-function App() {
+import './styles.css';
+
+
+export function App() {
 
   return (
-   <h1>Hello word</h1>
+    <div>
+     <Header />
+
+    <Post
+      author="Diego Fernandes"
+      content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+    />
+    <Post
+      author="Gabriel Buzzi"
+      content="Um novo post muito legal"
+    />
+    </div>
   )
 }
 
