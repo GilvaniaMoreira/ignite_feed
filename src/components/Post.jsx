45f1cf9b8b4e680ee405ea3a784d8d699c40cb47
gilvania moreira <moreiragil84@gmail.com>
2024-04-03 11:18:22 -0300

    feat: communication between components

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,6 +34,9 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {
+    console.log(`Deletar comentário ${comment}`)
+  }
 
   return (
     <article className={styles.post}>
@@ -56,9 +59,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
       {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
@@ -81,7 +84,13 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.commentList}>
         <Comment />
         {comments.map(comment => {
-          return <Comment content={comment}/>
+          return (
+            <Comment
+            key={comment}
+            content={comment}
+            onDeleteComment={deleteComment}
+          />
+        )
         })}
       </div>
     </article>
